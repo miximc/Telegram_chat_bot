0a2efcbdf1a46480141890f9ffb15a3bce176c7b
mixi <miximc@mail.ru>
2023-02-21 17:28:16 +0300

    Добавлена работата с файлами и ответ на голос

--- bot_v3.py
+++ bot_v3.py
@@ -3,17 +3,18 @@ from secret import token
 from telebot import types
 from datetime import datetime
 
+all_content_types = ["text", "audio", "document", "photo", "sticker", "video", "video_note", "voice", "location", "contact",
+                 "new_chat_members", "left_chat_member", "new_chat_title", "new_chat_photo", "delete_chat_photo",
+                 "group_chat_created", "supergroup_chat_created", "channel_chat_created", "migrate_to_chat_id",
+                 "migrate_from_chat_id", "pinned_message"]
 
-ready_list = []
-not_ready_list = []
 tooday_datetime = datetime.now()
 bot = telebot.TeleBot(token)
 
+def make_str(ready):
+    a_str = "".join(ready)
+    return a_str   
 
-
-def qwe(ready):
-    a = "".join(ready)
-    return a   
 #___________________________________________________________________________
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -24,10 +25,9 @@ def start(message):
     markup.add(btn1, btn2)
     bot.send_message(message.chat.id, 'Hi!', reply_markup=markup)
 #___________________________________________________________________________
-
 @bot.message_handler(content_types=['text'])
 def user(message):
-    global a
+
     if message.text == 'Время и дата':
         bot.send_message(
             message.chat.id,
@@ -37,38 +37,46 @@ def user(message):
         btn1 = types.KeyboardButton('Выполненные дела')
         btn2 = types.KeyboardButton('Не выполненные дела')
         btn3 = types.KeyboardButton('Добавить выполненное дело')
-        btn4 = types.KeyboardButton('Добавить дело')  
-        markup.add(btn1, btn2, btn3,btn4)
+        btn4 = types.KeyboardButton('Добавить дело')
+        btn5 = types.KeyboardButton('Удалить все дела')  
+        markup.add(btn1, btn2, btn3, btn4, btn5)
         bot.send_message(message.chat.id, 'Выберете что вы хотите сделать:', reply_markup=markup) 
     elif message.text == 'Выполненные дела':
         try:
             file = open('my_list.txt')
-            a = []
+            a_list = []
             for i in file:
-                a.append(i)
-            print(a)
+                a_list.append(i)
             file.close()
-            bot.send_message(message.chat.id, qwe(a))
+            bot.send_message(message.chat.id, make_str(a_list))
         except:
-            bot.send_message(message.chat.id, 'Нет такого файла')
-            print('Такого файла нет или нет выполненных дел')
+            bot.send_message(message.chat.id, 'У вас нет выполненных дел')
+            f1 = open('my_list_2.txt', 'a')
+            file.close()
     elif message.text == 'Не выполненные дела':
         try:
             file_2 = open('my_list_2.txt')
-            a = []
+            a_list_2 = []
             for i in file_2:
-                a.append(i)
-            print(a)
+                a_list_2.append(i)
             file_2.close()
-            bot.send_message(message.chat.id, qwe(a))
+            bot.send_message(message.chat.id, make_str(a_list_2))
         except:
-            bot.send_message(message.chat.id, 'Нет такого файла')
-            print('Такого файла нет или нет выполненных дел')
+            bot.send_message(message.chat.id, 'У вас нет дел')
     
     elif message.text == 'Добавить дело':
-        
         mesg = bot.send_message(message.chat.id,'Введите ваше дело:')
-        bot.register_next_step_handler(mesg,user_input)
+        bot.register_next_step_handler(mesg, user_input)
+
+    elif message.text == 'Добавить выполненное дело':
+        mesg = bot.send_message(message.chat.id,'Введите ваше дело:')
+        bot.register_next_step_handler(mesg, user_input_2)
+    elif message.text == 'Удалить все дела':
+        f1 = open('my_list_2.txt', 'w')
+        f1.close()
+        f2 = open('my_list.txt', 'w')
+        f2.close()
+
 def user_input(message):
     f1 = open('my_list_2.txt', 'a')
     a = message.text
@@ -76,10 +84,18 @@ def user_input(message):
     f1.close()
     bot.send_message(message.chat.id,'Добавлено!')
 
+def user_input_2(message):
+    f1 = open('my_list.txt', 'a')
+    a = message.text
+    f1.write(a + '\n')
+    f1.close()
+    bot.send_message(message.chat.id,'Добавлено!')
+#_______________________________________________________________________________________
 
-
-            
-            
-        
-                
+@bot.message_handler(content_types=all_content_types)
+def audio_user(message):
+    if message.content_type == 'voice':
+        bot.send_photo(message.chat.id, open('screenshot_18-2.png', 'rb'))
+        bot.send_message(message.chat.id, 'Чего блин?\nЯ не могу распознать эту запись')
+#_______________________________________________________________________________________
 bot.polling(none_stop=True)
